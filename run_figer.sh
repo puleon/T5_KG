e19861671cc8a36bac0722c5e996cceca2ba1915
Леонид Пугачев <pugachev@dgx2.ipavlov.mipt.ru>
2022-03-25 19:46:23 +0300

    add files

---
+++ run_figer.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+source ~/envs/transformers_new/bin/activate
+
+export CUDA_VISIBLE_DEVICES=6,7
+
+task=figer
+model_dir=./trained_models/t5_${task}_padtomaxlenF_wtrip
+dt=$(date '+%d.%m.%Y_%H.%M.%S')
+mkdir $model_dir
+cp ./run_figer.sh $model_dir/run_figer.sh_$dt
+cp ./run_summarization_entity_typing.py $model_dir/run_summarization_entity_typing.py_$dt
+
+python ./run_summarization_entity_typing.py \
+                --model_name_or_path t5-small \
+                --cache_dir ./downloaded_models \
+                --output_dir $model_dir \
+\
+                --text_column input \
+                --summary_column label \
+                --task_name $task \
+                --train_file ./data/${task}_with_triplets_json/train.json \
+                --validation_file ./data/${task}_with_triplets_json/dev.json \
+                --labels_file ./data/${task}_json/labels.json \
+                --preprocessing_num_workers 20 \
+                --max_source_length 256 \
+                --max_target_length 64 \
+                --val_max_target_length 64 \
+                --source_prefix  "" \
+                --predict_with_generate True \
+                --evaluation_strategy epoch \
+\
+                --do_train \
+                --do_eval \
+                --per_device_train_batch_size 64 \
+                --per_device_eval_batch_size 64 \
+                --learning_rate 1e-3 \
+                --num_train_epochs 5.0 \
+                --logging_strategy steps \
+                --log_level info \
+                --logging_dir $model_dir \
+                --logging_steps 1500 \
+                --logging_first_step True \
+                --save_strategy steps \
+                --save_steps 7500 \
+                --evaluation_strategy steps \
+                --eval_steps 7500 \
+                --pad_to_max_length False \
+\
+                --do_predict True \
+                --load_best_model_at_end True \
+                --test_file ./data/${task}_with_triplets_json/test.json
+                
+
+
+# --max_predict_samples 512
+# --eval_steps 500 \
+# --save_steps 500 \
+# --overwrite_output_dir True \
+# --overwrite_cache True \
+# --max_train_samples 512 \
+# --max_eval_samples 512 \
+
---
+++ run_generate_triplets.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+source ~/envs/transformers_new/bin/activate
+
+export CUDA_VISIBLE_DEVICES=6,7
+
+task=figer
+model_dir=./trained_models/t5_trex_pretrain_padtomaxlenF_generate_triplets
+dt=$(date '+%d.%m.%Y_%H.%M.%S')
+
+mkdir $model_dir
+cp ./run_generate_triplets.sh $model_dir/run_generate_triplets.sh_$dt
+cp .run_summarization_predict.py $model_dir/run_summarization_predict.py_$dt
+
+for set in train dev test
+do
+	python ./run_summarization_predict.py \
+			--model_name_or_path t5-small \
+			--cache_dir ./downloaded_models \
+			--output_dir $model_dir \
+	\
+			--text_column input \
+			--summary_column label \
+			--train_file ./data/trex_json/train.json \
+			--validation_file ./data/trex_json/dev.json \
+			--preprocessing_num_workers 20 \
+			--max_source_length 256 \
+			--max_target_length 64 \
+			--val_max_target_length 64 \
+			--source_prefix  "" \
+			--predict_with_generate True \
+			--do_train False \
+			--do_eval False \
+			--evaluation_strategy no \
+			--save_strategy no \
+	\
+			--per_device_train_batch_size 64 \
+			--per_device_eval_batch_size 64 \
+			--learning_rate 1e-3 \
+			--num_train_epochs 3.0 \
+			--logging_strategy steps \
+			--log_level info \
+			--logging_dir $model_dir \
+			--logging_steps 100 \
+			--logging_first_step True \
+			--pad_to_max_length False \
+	\
+			--load_best_model_at_end True \
+			--test_file ./data/${task}_json/${set}.json \
+			--do_predict True \
+			--generated_predictions_file generated_predictions_${task}_${set}.txt
+done
+# --max_predict_samples 512
+# --overwrite_output_dir True \
+# --overwrite_cache True \
+# --max_train_samples 512 \
+# --max_eval_samples 512 \
+
