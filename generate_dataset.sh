5127db8efd96aa582754c8a628eb29fdc0f8de47
puleon <puleon@mail.ru>
2022-04-19 20:05:54 +0300

    change parameters in sh scripts

--- generate_dataset.sh
+++ generate_dataset.sh
@@ -2,9 +2,10 @@
 
 python ./save_dataset_to_t5_json.py \
 		--use_triplets True \
+		--triplets_dir ./trained_models/t5base_trex_pretrain_padtomaxlenF_dsp_shddpsimple_TRIPLETS \
 		--function_id 1 \
 		--data_dir ./data/OpenEntity \
-		--output_dir ./data/openentity_with_spec_tok_json \
+		--output_dir ./data/openentity_with_triplets_base_new_json \
 		--task_name openentity
 
 # --data_dir ./data/trex-rc \
