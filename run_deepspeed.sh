07e71353cd18037f937604b4c9592a3758995e3c
puleon <puleon@mail.ru>
2022-04-19 18:29:07 +0300

    change settings for openentity, add deepsped, requirements

--- run_deepspeed.sh
+++ run_deepspeed.sh
@@ -2,26 +2,26 @@
 
 #source ~/envs/transformers_new/bin/activate
 
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 
-task=openentity
-model_dir=./trained_models/t5base_${task}_padtomaxlenF_bmf1_adaf
+model_dir=./trained_models/t5base_trex_pretrain_padtomaxlenF_dsp_shddpsimple
 dt=$(date '+%d.%m.%Y_%H.%M.%S')
-mkdir $model_dir
-cp ./run_openentity.sh $model_dir/run_openentity.sh_$dt
-cp ./run_summarization_finetune.py $model_dir/run_summarization_finetune.py_$dt
 
-python ./run_summarization_finetune.py \
+mkdir $model_dir
+cp ./run_deepspeed.sh $model_dir/run_deepspeed.sh_$dt
+cp ./run_summarization.py $model_dir/run_summarization.py_$dt
+
+deepspeed ./run_summarization.py \
+		--sharded_ddp "simple" \
                 --model_name_or_path t5-base \
                 --cache_dir ./downloaded_models \
                 --output_dir $model_dir \
+                --overwrite_output_dir True \
 \
                 --text_column input \
                 --summary_column label \
-                --task_name $task \
-                --train_file ./data/${task}_json/train.json \
-                --validation_file ./data/${task}_json/dev.json \
-                --labels_file ./data/${task}_json/labels.json \
+                --train_file ./data/trex_json/train.json \
+                --validation_file ./data/trex_json/dev.json \
                 --preprocessing_num_workers 20 \
                 --max_source_length 256 \
                 --max_target_length 64 \
@@ -35,27 +35,21 @@ python ./run_summarization_finetune.py \
                 --per_device_train_batch_size 32 \
                 --per_device_eval_batch_size 32 \
                 --learning_rate 1e-3 \
-                --optim adafactor \
-                --num_train_epochs 10.0 \
+                --num_train_epochs 5.0 \
                 --logging_strategy steps \
                 --log_level info \
                 --logging_dir $model_dir \
-                --logging_steps 10 \
+                --logging_steps 100 \
                 --logging_first_step True \
                 --save_strategy epoch \
                 --evaluation_strategy epoch \
                 --pad_to_max_length False \
 \
-                --do_predict True \
                 --load_best_model_at_end True \
-		--metric_for_best_model f1_micro \
-                --test_file ./data/${task}_json/test.json
-                
+                --test_file ./data/trex_json/dev.json \
+                --do_predict True \
+                --max_predict_samples 512
 
-# --lr_scheduler_type constant \
-# --max_predict_samples 512 \
-# --eval_steps 500 \
-# --save_steps 500 \
 # --overwrite_output_dir True \
 # --overwrite_cache True \
 # --max_train_samples 512 \
